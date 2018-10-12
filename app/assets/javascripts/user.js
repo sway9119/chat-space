55309cfadc1fcfd3a20f3ea01bb8926349812d6c
sway <fear.and.sway@gmail.com>
2018-10-12 16:06:03 +0900

    Add user.js

---
+++ app/assets/javascripts/user.js
@@ -0,0 +1,27 @@
+$(function(){
+
+  $("#user-search-field").on("keyup", function() {
+    var input = $("#user-search-field").val();
+    $.ajax({
+      type: 'GET',
+      url: '/users',
+      data: {keyword: input},
+      dataType: 'json'
+    })
+    .done(function(users){
+      $("#user-search-result").empty();
+        if (users.length !== 0) {
+          users.forEach(function(user){
+            appendUser(user);
+          });
+        }
+        else {
+          appendNoUser("一致するユーザーはいません");
+        }
+      })
+      .fail(function() {
+        alert('ユーザー検索に失敗しました');
+      })
+    });
+
+});
