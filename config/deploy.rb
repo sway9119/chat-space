75168708791009babd8a66611852daee084c7a85
sway <fear.and.sway@gmail.com>
2018-10-13 13:56:38 +0900

    Fix

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid only for current version of Capistrano
-lock '<Capistranoのバージョン>'
+lock '3.11.0'
 
 set :application, 'chat-space'
 set :repo_url,  'git@github.com:sway9119/chat-space.git'
